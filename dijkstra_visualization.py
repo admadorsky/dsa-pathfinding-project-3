732095291be4b5ed2b11d9eca785f9d704e098dc
anthony madorsky <127152790+admadorsky@users.noreply.github.com>
2023-11-26 02:50:26 -0500

    indexed adj_list in main.py & edges in viz

--- dijkstra_visualization.py
+++ dijkstra_visualization.py
@@ -9,14 +9,13 @@ folder = pathlib.Path(file_loc).parent.resolve()
 # open nodes.csv and store it in object "nodes"
 nodes = pathlib.Path(folder).joinpath('nodes.csv')
 # open adj_list_with_weights.csv and store it in object "adj_list"
-adj_list = pathlib.Path(folder).joinpath('adj_list_with_weights.csv')
+adj_list = pathlib.Path(folder).joinpath('adj_list.csv')
 
 collection = bpy.data.collections['Collection']
 
 # initialize storage
 vertices = []
 edges = []
-faces = []
 
 # read vertex/intersection data
 with open(nodes, 'r') as csvfile:
@@ -30,8 +29,24 @@ with open(nodes, 'r') as csvfile:
         vertices.append((float(row[1]) * 10, float(row[2]) * 10, 0))
         i = i + 1
         
+with open(adj_list, 'r') as csvfile2:
+    # initialize csv reader object
+    datareader2 = csv.reader(csvfile2)
+    # skip header
+    next(datareader2)
+    # initialize edge counter
+    i2 = 0
+    for row in datareader2:
+        neighbors = row[1].split()
+        neighbors[0] = neighbors[0][1:]
+        
+        for neighbor in neighbors:
+            if len(neighbor) > 1:
+                edges.append((int(row[0]), int(neighbor[:-1])))
+                i2 = i2 + 1
+        
 new_mesh = bpy.data.meshes.new('data')
-new_mesh.from_pydata(vertices, edges, faces)
+new_mesh.from_pydata(vertices, edges, [])
 new_mesh.update()
 
 print(i)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib
 import csv
 from Dijkstra import Dijkstra
 
+
 def main():
     G = ox.graph_from_place("Gainesville, Florida, USA", network_type="drive")
     M = ox.utils_graph.get_undirected(G)
@@ -52,36 +53,65 @@ def main():
             # write data
             writer.writerow({'node_id': node, 'x': new_x, 'y': new_y})
 
-    # Initialize an empty dictionary to store the adjacency list with weights
+    # Initialize an empty dictionary to store the adjacency lists
     adj_list_with_weights = {}
+    adj_list = {}
+    adj_list_i = {}
+    indeces = {}
+    # Initialize index counter
+    n = 0
 
     # Iterate over nodes in the graph
     for node in G.nodes():
+        indeces[node] = n
         # Get the neighbors and edge attributes for each node
         neighbors_and_weights = []
+        neighbors = []
         for neighbor, data in G[node].items():
             # Extract 'length' from the AtlasView
             length = data.get(0, {}).get('length', None)
             if length is not None:
                 neighbors_and_weights.append((neighbor, length))
+            neighbors.append((neighbor))
 
-        # Add the node and its list of neighbors with weights to the dictionary
+        # Add the node and its list of neighbors with weights to each dictionary
         adj_list_with_weights[node] = neighbors_and_weights
+        adj_list[node] = neighbors
 
-    # Write adjacency list with weights to a CSV file
-    with open('adj_list_with_weights.csv', 'w', newline='') as csvfile:
-        fieldnames = ['node_id', 'neighbors_with_weights']
+        n = n + 1
+
+    for node2 in adj_list:
+        indexed_neighbors = []
+        for neighbor2 in adj_list[node2]:
+            indexed_neighbors.append(indeces[neighbor2])
+        key = indeces[node2]
+        adj_list_i[key] = indexed_neighbors
+
+    # Write indexed adjacency list without weights to a CSV file
+    with open('adj_list.csv', 'w', newline='') as csvfile:
+        fieldnames = ['node_index', 'neighbors\' indeces']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         # Write the header
         writer.writeheader()
 
-        # Write adjacency list with weights data
-        for node, neighbors_and_weights in adj_list_with_weights.items():
-            writer.writerow({'node_id': node, 'neighbors_with_weights': neighbors_and_weights})
+        # Write adjacency list
+        for node3, neighbors in adj_list_i.items():
+            writer.writerow({'node_index': node3, 'neighbors\' indeces': neighbors})
 
-    result_to_visualize = Dijkstra(adj_list_with_weights, 84714023, 2654104462, 9999999999999999)
+    # Write adjacency list with weights to a CSV file
+    with open('adj_list_with_weights.csv', 'w', newline='') as csvfile2:
+        fieldnames2 = ['node_id', 'neighbors_with_weights']
+        writer2 = csv.DictWriter(csvfile2, fieldnames=fieldnames2)
+
+        # Write the header
+        writer2.writeheader()
+
+        # Write adjacency list with weights data
+        for node4, neighbors_and_weights in adj_list_with_weights.items():
+            writer2.writerow({'node_id': node4, 'neighbors_with_weights': neighbors_and_weights})
+
+    # result_to_visualize = Dijkstra(adj_list_with_weights, 84714023, 2654104462, 9999999999999999)
 
 if __name__ == '__main__':
     main()
-   
